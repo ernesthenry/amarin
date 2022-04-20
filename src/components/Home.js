283a3aca594fb622db5882804a9fec0fa5555402
ernesthenry <henry38ernest@gmail.com>
2022-04-20 10:07:41 +0300

    API PATH

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ const HomeScreen = () => {
     // this useEffect will run once
     // similar to componentDidMount()
     useEffect(() => {
-      fetch("https://api.example.com/items")
+      fetch("api/")
         .then(res => res.json())
         .then(
           (result) => {
